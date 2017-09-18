006eb2db7d094fd33d47df3e4a80e5e5117da86e
Xiao Xiao <xx2004xiamen@gmail.com>
2017-09-18 20:46:21 +0900

    Add support for Carthage

---
+++ CTFeedbackDemo/CTFeedback/CTFeedback.h
@@ -0,0 +1,19 @@
+//
+//  CTFeedback.h
+//  CTFeedback
+//
+//  Created by Xiao Xiao on 平成29-09-18.
+//  Copyright © 平成29年 CAPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CTFeedback.
+FOUNDATION_EXPORT double CTFeedbackVersionNumber;
+
+//! Project version string for CTFeedback.
+FOUNDATION_EXPORT const unsigned char CTFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CTFeedback/PublicHeader.h>
+
+
